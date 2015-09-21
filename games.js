995d432873d7d359f908d8121f9a62fe016e68df
Vinícius Thiengo <viniciusthiengo@gmail.com>
2015-09-21 16:20:21 -0300

    Initial commit

---
+++ games.js
@@ -0,0 +1,109 @@
+/**
+ * Created by viniciusthiengo on 9/21/15.
+ */
+
+// com.parse.Data
+var comDotParseDotData = {
+    "title": "SoccerTeam",
+    "alert": "Flamengo 11 x 5 Barcelona - Gollll! Do Flamengo",
+    "data":{
+            "games":[
+                {
+                    "home":{
+                        "name":"Flamengo",
+                        "score":11,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/flamengo.png"
+                    },
+                    "guest":{
+                        "name":"Barcelona",
+                        "score":5,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/barcelona.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"Chelsea",
+                        "score":1,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/chelsea.png"
+                    },
+                    "guest":{
+                        "name":"Corinthians",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/corinthians.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"Arsenal",
+                        "score":5,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/arsenal.png"
+                    },
+                    "guest":{
+                        "name":"Zenit",
+                        "score":3,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/zenit.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"Real Madrid",
+                        "score":2,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/real-madrid.png"
+                    },
+                    "guest":{
+                        "name":"Manchester United",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/manchester-united.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"Ajax",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/ajax.png"
+                    },
+                    "guest":{
+                        "name":"Juventus",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/juventus.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"River Plate",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/river-plate.png"
+                    },
+                    "guest":{
+                        "name":"Borussia Dortmund",
+                        "score":1,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/borussia-dortmund.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"PSG",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/psg.png"
+                    },
+                    "guest":{
+                        "name":"Milan",
+                        "score":3,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/milan.png"
+                    }
+                },
+                {
+                    "home":{
+                        "name":"Manchester City",
+                        "score":0,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/manchester-city.png"
+                    },
+                    "guest":{
+                        "name":"Atlético de Madrid",
+                        "score":3,
+                        "urlLogo":"http://192.168.25.221:8888/ParseNotificationExample/img/atletico-madrid.png"
+                    }
+                }
+        ]
+    }
+}
